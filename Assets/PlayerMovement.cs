2e45f39bd53d309e8b2461cea8d4023596d398f8
Thyago Stall <thstall@gmail.com>
2023-05-21 13:50:13 -0300

    Adding animation for idle and run, and also add transitions

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D playerRigidbody;
+    private Animator playerAnimator;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float directionX = Input.GetAxisRaw("Horizontal");
-
         playerRigidbody.velocity = new Vector2(7f * directionX, playerRigidbody.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 14f);
+        }
+
+        if (directionX > 0f)
+        {
+            playerAnimator.SetBool("running", true);
+        }
+        else if (directionX < 0f)
+        {
+            playerAnimator.SetBool("running", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("running", false);
         }
     }
 }
