959838d66b3674bd5678f0abca7b842f9f931ba1
Thyago Stall <thstall@gmail.com>
2023-05-21 20:06:32 -0300

    Adding new font + updating text with cherries number

--- Assets/Pixel Adventure 1/Scripts/ItemCollector.cs
+++ Assets/Pixel Adventure 1/Scripts/ItemCollector.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
+
+    [SerializeField] private Text cherriesText;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,9 +16,8 @@ public class ItemCollector : MonoBehaviour
             return;
         }
 
-        Destroy(collision.gameObject);
         cherries++;
-
-        Debug.Log("Cherries: " + cherries);
+        cherriesText.text = "Cherries: " + cherries;
+        Destroy(collision.gameObject);
     }
 }
