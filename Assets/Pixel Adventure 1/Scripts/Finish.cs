1efe237d41beea24022d16bc80751950452238ce
Thyago Stall <thstall@gmail.com>
2023-05-31 23:36:22 -0300

    Go to the next when complete level

--- Assets/Pixel Adventure 1/Scripts/Finish.cs
+++ Assets/Pixel Adventure 1/Scripts/Finish.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
@@ -24,6 +25,6 @@ public class Finish : MonoBehaviour
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
