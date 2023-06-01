7a63a8a01e3f941a395769d60d2418892576f197
Thyago Stall <thstall@gmail.com>
2023-06-01 09:54:07 -0300

    Iterating on camera to avoid showing scene offlimits

--- Assets/Pixel Adventure 1/Scripts/CameraController.cs
+++ Assets/Pixel Adventure 1/Scripts/CameraController.cs
@@ -6,8 +6,35 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
 
+    [SerializeField] private Transform bottomLeftLimit;
+    [SerializeField] private Transform bottomRightLimit;
+
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
     private void Update()
     {
-        transform.position = new Vector3(playerTransform.position.x, Mathf.Max(playerTransform.position.y, 0), transform.position.z);
+        float x = IsCameraExceedingXLimit() ? transform.position.x : playerTransform.position.x;
+        float y = IsCameraExceedingYLimit() ? transform.position.y : playerTransform.position.y;
+
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+
+    private bool IsCameraExceedingXLimit()
+    {
+        float cameraWidth = camera.orthographicSize * camera.aspect;
+
+        return playerTransform.position.x - cameraWidth < bottomLeftLimit.position.x
+            || playerTransform.position.x + cameraWidth > bottomRightLimit.position.x;
+    }
+
+    private bool IsCameraExceedingYLimit()
+    {
+        return playerTransform.position.y - camera.orthographicSize < bottomLeftLimit.position.y
+            || playerTransform.position.y - camera.orthographicSize < bottomRightLimit.position.y;
     }
 }
